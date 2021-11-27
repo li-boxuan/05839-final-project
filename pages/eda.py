c8d16b8c839e8f5b844250a831ec8884d203865a
Boxuan Li <liboxuan@connect.hku.hk>
2021-11-26 20:19:11 -0500

    Add EDA page

--- pages/eda.py
+++ pages/eda.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
 
 def write():
     st.header("Exploratory Data Analysis")
     st.write("In this section, we clean & analyze the original datasets")
+    st.subheader("Demography")
+    components.html(open("./eda/eda_demography.html", "r", encoding='utf-8').read(), height=4800)
+    st.subheader("Economics")
+    components.html(open("./eda/economic_data_eda.html", "r", encoding='utf-8').read(), height=2500)
+    st.subheader("Education")
+    components.html(open("./eda/education.html", "r", encoding='utf-8').read(), height=3300)
+    st.subheader("Housing")
+    components.html(open("./eda/housing.html", "r", encoding='utf-8').read(), height=3000)
+    # TODO: cannot run climate.ipynb
